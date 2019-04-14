3d5a9e3eadb703a15cfe421c18bbd2dec79ccd04
Javon Lee <javonlee@Javons-MacBook-Pro.local>
2019-04-13 21:06:22 -0400

    Added Routing, Bootstrap Styles, and Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { FloorPlansComponent } from './components/floor-plans/floor-plans.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { AmenitiesComponent } from './components/amenities/amenities.component';
+import { AboutComponent } from './components/about/about.component'
+import { HomeComponent } from './components/home/home.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'floor-plans', component: FloorPlansComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'amenities', component:AmenitiesComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FloorPlansComponent } from './components/floor-plans/floor-plans.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { AmenitiesComponent } from './components/amenities/amenities.component';
+import { AboutComponent } from './components/about/about.component'
+import { HomeComponent } from './components/home/home.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FloorPlansComponent,
+    ContactUsComponent,
+    AmenitiesComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
